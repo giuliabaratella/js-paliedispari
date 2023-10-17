1eeb8e6fa0f23a70fe2746f1c6caa6d78f234ea3
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-17 15:26:16 +0200

    added btn event + array + array reverse

--- js/script.js
+++ js/script.js
@@ -2,6 +2,37 @@
 // Chiedere all’utente di inserire una parola,
 // Creare una funzione per capire se la parola inserita è palindroma.
 
+const btn = document.getElementById('button');
+const inputBox = document.getElementById ('data');
+const inputBoxLabel = document.getElementById('data-label');
+
+inputBoxLabel.innerHTML= 'Inserisci una parola e verifica se è palindroma'
+
+// creo un array vuoto che conterrà le lettere della parola da analizzare 
+let parolaArray = [];
+
+// creo l'evento al click del bottone 
+btn.addEventListener('click', function() {
+
+    // raccolgo la parola scritta dall'utente 
+    let parola = inputBox.value ;
+    console.log (parola);
+
+    // ciclo per ogni lettera della parola scritta che inserisce una lettera nell'array
+    for (let i = 0; i < parola.length; i++){
+        parolaArray.push (parola[i]);
+    }
+
+    // controllo l'array e il suo reverse 
+    console.log (parolaArray);
+    console.log(parolaArray.reverse())
+
+
+}
+)
+
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer.
