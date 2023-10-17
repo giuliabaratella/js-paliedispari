e2fca87983bb289495bd0b1685f6de68586cb3a7
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-17 14:28:02 +0200

    added html + css + js files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola,
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer.
+// Sommiamo i due numeri,
+// Stabiliamo se la somma dei due numeri è pari o dispari,
+// Dichiariamo chi ha vinto.
+
+// utility
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
